7bae6af01b863d8f1e627687c8119a987f49d092
George Hilliard <thirtythreeforty@gmail.com>
2020-04-18 21:31:49 -0500

    Implement initial login flow

--- src/bc/xml.rs
+++ src/bc/xml.rs
@@ -21,6 +21,8 @@ pub struct Body {
     pub login_user: Option<LoginUser>,
     ***REMOVED***[yaserde(rename="LoginNet")]
     pub login_net: Option<LoginNet>,
+    ***REMOVED***[yaserde(rename="DeviceInfo")]
+    pub device_info: Option<DeviceInfo>,
 }
 
 impl Body {
@@ -71,6 +73,21 @@ impl Default for LoginNet {
         }
     }
 }
+
+***REMOVED***[derive(PartialEq, Eq, Default, Debug, YaDeserialize, YaSerialize)]
+pub struct DeviceInfo {
+    ***REMOVED***[yaserde(rename="Resolution")]
+    pub resolution: Resolution,
+}
+
+***REMOVED***[derive(PartialEq, Eq, Default, Debug, YaDeserialize, YaSerialize)]
+pub struct Resolution {
+    ***REMOVED***[yaserde(rename="resolutionName")]
+    pub name: String,
+    pub width: u32,
+    pub height: u32,
+}
+
 
 pub fn xml_ver() -> String {
     "1.1".to_string()
