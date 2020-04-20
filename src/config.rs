861dfe68b97a8a7f93fd9ab939ea6dbc90b244ef
George Hilliard <thirtythreeforty@gmail.com>
2020-04-20 14:18:47 -0500

    Implement configuration file, with multiple cameras

---
+++ src/config.rs
@@ -0,0 +1,48 @@
+use serde::Deserialize;
+use std::fmt;
+use std::net::SocketAddr;
+use std::net::ToSocketAddrs;
+
+***REMOVED***[derive(Debug, Deserialize)]
+pub struct Config {
+    pub cameras: Vec<CameraConfig>,
+}
+
+***REMOVED***[derive(Debug, Deserialize)]
+pub struct CameraConfig {
+    pub name: String,
+
+    ***REMOVED***[serde(rename="address")]
+    pub camera_addr: SocketAddr,
+
+    ***REMOVED***[serde(rename="serve")]
+    pub bind_addr: BindAddr,
+
+    pub stream: Option<String>,
+    pub username: String,
+    pub password: Option<String>,
+}
+
+***REMOVED***[derive(Debug, PartialEq, Eq, Deserialize)]
+pub struct BindAddr {
+    host: Option<String>,
+    port: u16,
+}
+
+impl fmt::Display for BindAddr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let Some(ref host) = self.host {
+            write!(f, "{}:{}", host, self.port)
+        } else {
+            write!(f, "port {}", self.port)
+        }
+    }
+}
+
+impl ToSocketAddrs for BindAddr {
+    type Iter = std::vec::IntoIter<SocketAddr>;
+    fn to_socket_addrs(&self) -> std::io::Result<Self::Iter> {
+        let host = self.host.as_deref().unwrap_or("0.0.0.0");
+        (host, self.port).to_socket_addrs()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,56 @@
-***REMOVED***![allow(dead_code)] // while I'm still fleshing this out
-
-use err_derive::Error;
-use std::env::args;
-use std::net::TcpListener;
-
 mod bc;
 mod bc_protocol;
+mod config;
 
 use bc_protocol::BcCamera;
+use config::{Config, CameraConfig};
+use crossbeam_utils::thread;
+use err_derive::Error;
+use std::env::args;
+use std::fs;
+use std::net::TcpListener;
 
 ***REMOVED***[derive(Debug, Error)]
 pub enum Error {
+    ***REMOVED***[error(display="Configuration parsing error")]
+    ConfigError(***REMOVED***[error(source)] toml::de::Error),
     ***REMOVED***[error(display="Communication error")]
     ProtocolError(***REMOVED***[error(source)] bc_protocol::Error),
-    ***REMOVED***[error(display="Socket setup error")]
+    ***REMOVED***[error(display="I/O error")]
     IoError(***REMOVED***[error(source)] std::io::Error),
 }
 
 fn main() -> Result<(), Error> {
-    let mut camera = BcCamera::new_with_addr(args().nth(1).unwrap())?;
-    camera.connect()?;
-    camera.login("admin", Some("12345678"))?;
+    let config_path = args().nth(1).expect("A config file must be supplied on the command line");
+    let config: Config = toml::from_str(&fs::read_to_string(config_path)?)?;
 
-    let bind_addr = "0.0.0.0:9999";
-    println!("Logged in to camera; awaiting connection on {}", bind_addr);
+    thread::scope(|s| {
+        for camera in config.cameras {
+            s.spawn(move |_| {
+                // TODO handle these errors
+                camera_main(&camera)
+            });
+        }
+    }).unwrap();
+
+    Ok(())
+}
+
+fn camera_main(camera_config: &CameraConfig) -> Result<(), Error> {
+    let mut camera = BcCamera::new_with_addr(camera_config.camera_addr)?;
+
+    println!("{}: Connecting to camera at {}", camera_config.name, camera_config.camera_addr);
+
+    camera.connect()?;
+    camera.login(&camera_config.username, camera_config.password.as_deref())?;
+
+    let bind_addr = &camera_config.bind_addr;
+    println!("{}: Logged in to camera; awaiting connection on {}", camera_config.name, bind_addr);
 
     let listener = TcpListener::bind(bind_addr)?;
     let (mut out_socket, remote_addr) = listener.accept()?;
 
-    println!("Connected to {}, starting video stream", remote_addr);
+    println!("{}: Connected to {}, starting video stream", camera_config.name, remote_addr);
 
     camera.start_video(&mut out_socket)?;
 
