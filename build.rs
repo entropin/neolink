3b79783b1a4fbd251bd250c4f150a86ac34f7744
George Hilliard <thirtythreeforty@gmail.com>
2020-04-29 09:20:10 -0500

    Make it build on Windows
    Cribbed Windows script from
    https://github.com/nlowe/mousiki/commit/51bd789ec65fe87556ef3181cb388c26c3b3eb00

---
+++ build.rs
@@ -0,0 +1,14 @@
+***REMOVED***[cfg(windows)]
+fn main() {
+    use std::env;
+
+    let gstreamer_dir = env::var_os("GSTREAMER_1_0_ROOT_X86_64")
+        .and_then(|x| x.into_string().ok())
+        .unwrap_or_else(|| r***REMOVED***"C:\gstreamer\1.0\x86_64\"***REMOVED***.to_string());
+
+    println!(r"cargo:rustc-link-search=native={}\lib", gstreamer_dir);
+}
+
+***REMOVED***[cfg(not(windows))]
+fn main() {
+}
