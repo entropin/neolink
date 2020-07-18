200c70e2d821ac008df77c69e2efd53c06107565
George Hilliard <thirtythreeforty@gmail.com>
2020-07-18 14:56:11 -0500

    Refactor users to use &str as main type

--- src/config.rs
+++ src/config.rs
@@ -74,13 +74,12 @@ pub struct CameraConfig {
 
 ***REMOVED***[derive(Debug, Deserialize, Validate, Clone)]
 pub struct UserConfig {
-    ***REMOVED***[validate(required, custom = "validate_username")]
+    ***REMOVED***[validate(custom = "validate_username")]
     ***REMOVED***[serde(alias = "username")]
-    pub name: Option<String>,
+    pub name: String,
 
-    ***REMOVED***[validate(required)]
     ***REMOVED***[serde(alias = "password")]
-    pub pass: Option<String>,
+    pub pass: String,
 }
 
 fn default_bind_addr() -> String {
@@ -107,9 +106,12 @@ fn default_tls_client_auth() -> String {
     "none".to_string()
 }
 
+pub static RESERVED_NAMES: &[&str] = &["anyone", "anonymous"];
 fn validate_username(name: &str) -> Result<(), ValidationError> {
-    let reserved_names = vec!["anyone", "anonymous"];
-    if reserved_names.contains(&name) {
+    if name.trim().is_empty() {
+        return Err(ValidationError::new("username cannot be empty"))
+    }
+    if RESERVED_NAMES.contains(&name) {
         return Err(ValidationError::new("This is a reserved username"));
     }
     Ok(())
